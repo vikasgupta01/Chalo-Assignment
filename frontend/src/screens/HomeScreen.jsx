855a4307e157514cb1596c9d8f53cce3d6e00db2
vikasgupta01 <vikasgupta2552@gmail.com>
2022-04-05 20:14:04 +0530

    Update HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <p>
-        Welcome to Chalo webapp. Please use nav links to add/edit/view the stops
-        and routes.
-      </p>
+    <div className="float-container">
+      <div className="float-child">
+        <Image
+          src="https://chalo.com/assets/images/mock.png"
+          alt="Chalo App"
+          fluid
+        />
+      </div>
+      <div className="float-child">
+        <h3>The New Way Of Bus Travel</h3>
+        <h1>In Your City</h1>
+        <p>
+          Chalo is India’s #1 bus transport technology company. Live track your
+          bus on the Chalo App and get digital bus tickets on the Chalo Card.
+          Your one stop solution for a convenient and hassle-free travel
+          experience.
+        </p>
+        <Button className="btn btn-light my-3" href="https://chalo.com/app/">
+          Try Chalo
+        </Button>
+      </div>
     </div>
   );
 };
